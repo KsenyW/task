088caa279dfde66aab4fb43326bec9d1d845d8b1
ovolostnykh <volostnykhoksana@gmail.com>
2019-03-14 09:19:23 +0200

    snapshot for item

--- __tests__/helpersFunctions.test.js
+++ __tests__/helpersFunctions.test.js
@@ -10,25 +10,23 @@ const props = {
   locale: 'nl_NL'
 };
 
-describe('helper functions:', () => {  
-  it('processImgUrl function', () => {
-    // image height/width set to 500 by default
-    expect(processImgUrl('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-100-100.jpg'))
-          .toEqual('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-500-500.jpg');
-    
-    // has to change height/width according to passed params
-    expect(processImgUrl('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-100-100.jpg', '600', '600'))
-          .toEqual('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-600-600.jpg');
-  });
+test('helper functions: processImgUrl function', () => {  
+  // image height/width both set to 500 by default
+  expect(processImgUrl('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-100-100.jpg'))
+    .toEqual('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-500-500.jpg');
 
-  it('emulatePagination function', () => {
-    expect(emulatePagination(data, props))
-          .toHaveProperty('quantity', 85);
-    expect(emulatePagination(data, props))
-          .toHaveProperty('productsDataList');
-    expect(emulatePagination(data, props).productsDataList)
-          .toHaveLength(24); // for the first page
-    expect(emulatePagination(data, {...props, currentPage: 4}).productsDataList)
-          .toHaveLength(13); // for the last page in mocked data
-  });
+  // has to change height/width according to passed params
+  expect(processImgUrl('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-100-100.jpg', '600', '600'))
+    .toEqual('https://d16m3dafbknje9.cloudfront.net/imagescaler/8964674846750-600-600.jpg');
+});
+
+test('helper functions: emulatePagination function', () => {
+  expect(emulatePagination(data, props))
+    .toHaveProperty('quantity', 85);
+  expect(emulatePagination(data, props))
+    .toHaveProperty('productsDataList');
+  expect(emulatePagination(data, props).productsDataList)
+    .toHaveLength(24); // for the first page
+  expect(emulatePagination(data, {...props, currentPage: 4}).productsDataList)
+    .toHaveLength(13); // for the last page in mocked data
 });
